01c086ea691dcd5ec41db11540d0d1141c5dc285
ELETRAUD <122927068+ELETRAUD@users.noreply.github.com>
2023-06-06 16:50:59 +0300

    Update loginPage.js

---
+++ public/loginPage.js
@@ -0,0 +1,27 @@
+const userForm = new UserForm();
+userForm.loginFormCallback = (data) => {
+    ApiConnector.login(data, response => {
+        if (response.success) {
+            // console.log(data)
+            // console.log(response)
+            // console.log(response.success)
+            location.reload();
+            return
+        } else {
+            console.log(response.error);
+            userForm.setLoginErrorMessage(response.error);
+        }
+    })
+}
+
+userForm.registerFormCallback = (data) => {
+    ApiConnector.register(data, response => {
+        if (response.success) {
+            location.reload();
+            return
+        } else {
+            console.log(response.error);
+            userForm.setRegisterErrorMessage(response.error); 
+        }
+    })
+}
